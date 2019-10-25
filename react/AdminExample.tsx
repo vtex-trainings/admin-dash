288112c2f0dbbfa26d881ef57338d4eef2cbe81f
Sávio Muniz <savio.muniz@vtex.com.br>
2019-10-24 23:58:14 -0400

    styleguide

--- react/AdminExample.tsx
+++ react/AdminExample.tsx
@@ -7,6 +7,24 @@ import "./styles.global.css";
 class AdminExample extends Component {
   public render() {
     return (
+      <Layout
+      pageHeader={
+        <PageHeader
+          title={<FormattedMessage id="admin-example.hello-world" />}
+        />
+      }
+    >
+      <PageBlock variation="full">
+        <Input
+          label="Field 1"/>
+        <Input
+          label="Field 2"/>
+        <Input
+          label="Field 3"/>
+        <Input
+          label="Field 4"/>
+      </PageBlock>
+    </Layout>
     )
   }
 }
